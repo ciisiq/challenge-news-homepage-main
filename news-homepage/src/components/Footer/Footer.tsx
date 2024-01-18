9c51848bd8027ca3bf9cf213d585c29e5b41c4d7
ciisiq <cii_3@hotmail.com>
2024-01-18 11:08:26 +0000

    Style: finish footer

--- news-homepage/src/components/Footer/Footer.tsx
+++ news-homepage/src/components/Footer/Footer.tsx
@@ -1,13 +1,23 @@
+import './Footer.css';
+
 export default function Footer() {
   return (
     <div>
       <footer>
         <div className="attribution">
           Challenge by{' '}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a
+            className="footerLink"
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+          >
             Frontend Mentor
           </a>
-          . Coded by <a href="https://github.com/ciisiq">Cintia Siqueira</a>.
+          . Coded by{' '}
+          <a className="footerLink" href="https://github.com/ciisiq">
+            Cintia Siqueira
+          </a>
+          .
         </div>
       </footer>
     </div>
